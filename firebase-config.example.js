3954359378139d3c21cbaa4747973cc6bad5ec69
scallet16 <scallet16@gmail.com>
2026-07-15 00:54:51 +0900

    Firebase 실시간 데이터 동기화 적용

---
+++ firebase-config.example.js
@@ -0,0 +1,12 @@
+// 이 파일을 firebase-config.js로 복사한 뒤 Firebase Console의 웹 앱 설정값을 입력하세요.
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.REGION.firebasedatabase.app",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+export const defaultClassId = "default";
---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase Console > 프로젝트 설정 > 내 앱 > SDK 설정 및 구성
+export const firebaseConfig = {
+  apiKey: "AIzaSyCoxN_uAJJyYiqQ9BE39niBJj-_IIVUCtc",
+  authDomain: "martkethannae.firebaseapp.com",
+  databaseURL: "https://martkethannae-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "martkethannae",
+  storageBucket: "martkethannae.firebasestorage.app",
+  messagingSenderId: "266290636226",
+  appId: "1:266290636226:web:3ffd58567995a644c6625b",
+  measurementId: "G-YPNXQDQHPD"
+};
+
+export const defaultClassId = "default";
